ffa93807291ed6f38f852e665b35693c30b0d95e
闫野 <yanye@apkpure.net>
2020-11-24 18:32:26 +0800

    terminal init

--- 01-api/vue-auto-router-cli/bin/kkb.js
+++ 01-api/vue-auto-router-cli/bin/kkb.js
@@ -3,6 +3,13 @@ const { program } = require('commander')
 //-v
 program.version(require('../package.json').version)
 
+program
+    .command('init <name>')
+    .description('初始化项目')
+    .action(() =>{
+        console.log('hi .....');
+    })
+
 
 //解析参数
 program.parse(process.argv)
